973d33b72f92282db48f64a7578583588c30c8c0
noarche <n0arch@pm.me>
2024-12-29 14:01:42 -0500

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -4,9 +4,29 @@ from urllib.parse import urljoin, urlparse
 from colorama import Fore, Style, init
 import time
 import random
-
-# Assuming user_agents.py contains a list named USER_AGENTS
 from user_agents import USER_AGENTS
+
+main_logo = ''' 
+
+ [91m_[0m[93m_[0m[92m_[0m[96m_[0m           [94m_[0m[95m_[0m[91m_[0m[93m_[0m[92m_[0m[96m_[0m               [94m_[0m[95m_[0m              [91m_[0m[93m_[0m[92m_[0m[96m_[0m       
+[94m/[0m[95m\[0m  [91m_[0m[93m`[0m[92m\[0m        [96m/[0m[94m\[0m  [95m_[0m  [91m\[0m             [93m/[0m[92m\[0m [96m\[0m            [94m/[0m[95m\[0m  [91m_[0m[93m`[0m[92m\[0m     
+[96m\[0m [94m\[0m [95m\[0m[91m/[0m[93m\[0m[92m_[0m[96m\[0m  [94m_[0m [95m_[0m[91m_[0m[93m\[0m [92m\[0m [96m\[0m[94mL[0m[95m\[0m [91m\[0m  [93m_[0m[92m_[0m  [96m_[0m[94m_[0m  [95m_[0m[91m_[0m[93m\[0m [92m\[0m [96m\[0m         [94m_[0m[95m_[0m[91m\[0m [93m\[0m [92m\[0m[96mL[0m[94m\[0m [95m\[0m   
+ [91m\[0m [93m\[0m [92m\[0m[96m/[0m[94m_[0m[95m/[0m[91m_[0m[93m/[0m[92m\[0m[96m`[0m[94m'[0m[95m_[0m[91m_[0m[93m\[0m [92m\[0m  [96m_[0m[94m_[0m [95m\[0m[91m/[0m[93m\[0m [92m\[0m[96m/[0m[94m\[0m [95m\[0m[91m/[0m[93m\[0m [92m\[0m[96m\[0m [94m\[0m [95m\[0m  [91m_[0m[93m_[0m  [92m/[0m[96m'[0m[94m_[0m[95m_[0m[91m`[0m[93m\[0m [92m\[0m [96m,[0m  [94m/[0m   
+  [95m\[0m [91m\[0m [93m\[0m[92mL[0m[96m\[0m [94m\[0m [95m\[0m [91m\[0m[93m/[0m [92m\[0m [96m\[0m [94m\[0m[95m/[0m[91m\[0m [93m\[0m [92m\[0m [96m\[0m[94m_[0m[95m/[0m [91m\[0m[93m_[0m[92m/[0m [96m\[0m[94m\[0m [95m\[0m [91m\[0m[93mL[0m[92m\[0m [96m\[0m[94m/[0m[95m\[0m  [91m_[0m[93m_[0m[92m/[0m[96m\[0m [94m\[0m [95m\[0m[91m\[0m [93m\[0m  
+   [92m\[0m [96m\[0m[94m_[0m[95m_[0m[91m_[0m[93m_[0m[92m/[0m[96m\[0m [94m\[0m[95m_[0m[91m\[0m  [93m\[0m [92m\[0m[96m_[0m[94m\[0m [95m\[0m[91m_[0m[93m\[0m [92m\[0m[96m_[0m[94m_[0m[95m_[0m[91mx[0m[93m_[0m[92m_[0m[96m_[0m[94m/[0m[95m'[0m [91m\[0m [93m\[0m[92m_[0m[96m_[0m[94m_[0m[95m_[0m[91m/[0m[93m\[0m [92m\[0m[96m_[0m[94m_[0m[95m_[0m[91m_[0m[93m\[0m[92m\[0m [96m\[0m[94m_[0m[95m\[0m [91m\[0m[93m_[0m[92m\[0m
+    [96m\[0m[94m/[0m[95m_[0m[91m_[0m[93m_[0m[92m/[0m  [96m\[0m[94m/[0m[95m_[0m[91m/[0m   [93m\[0m[92m/[0m[96m_[0m[94m/[0m[95m\[0m[91m/[0m[93m_[0m[92m/[0m[96m\[0m[94m/[0m[95m_[0m[91m_[0m[93m/[0m[92m/[0m[96m_[0m[94m_[0m[95m/[0m    [91m\[0m[93m/[0m[92m_[0m[96m_[0m[94m_[0m[95m/[0m  [91m\[0m[93m/[0m[92m_[0m[96m_[0m[94m_[0m[95m_[0m[91m/[0m [93m\[0m[92m/[0m[96m_[0m[94m/[0m[95m\[0m[91m/[0m [93m/[0m                                                              
+\033[96m   github.com/noarche/crawler   \033[0m                                                                
+\033[96m   Build Date: Dec. 29 2024\033[0m
+'''
+infoabt = ''' 
+ \033[92m  This script crawls links. Originally created to gather links for Noisy.py\033[0m
+ \033[32m   To view noisy script visit github.com/noarche/noisy\033[0m
+ \033[96m   Enter a starting link..\033[0m
+ \033[36m   Enter a sleep time. Leave blank and press enter for very fast.\033[0m
+'''
+
+print(main_logo)
+print(infoabt)
 
 init(autoreset=True)
 
@@ -76,10 +96,45 @@ def crawl_website(url, visited_links, links_to_visit):
             if parsed_url.scheme in ('http', 'https') and (
                 parsed_url.netloc.endswith('.net') or
                 parsed_url.netloc.endswith('.com') or
+                parsed_url.netloc.endswith('.co') or
                 parsed_url.netloc.endswith('.edu') or
                 parsed_url.netloc.endswith('.org') or
                 parsed_url.netloc.endswith('.gov') or
                 parsed_url.netloc.endswith('.io') or
+                parsed_url.netloc.endswith('.gg') or
+                parsed_url.netloc.endswith('.blog') or
+                parsed_url.netloc.endswith('.city') or
+                parsed_url.netloc.endswith('.club') or
+                parsed_url.netloc.endswith('.online') or
+                parsed_url.netloc.endswith('.ai') or
+                parsed_url.netloc.endswith('.eu') or
+                parsed_url.netloc.endswith('.int') or
+                parsed_url.netloc.endswith('.mil') or
+                parsed_url.netloc.endswith('.us') or
+                parsed_url.netloc.endswith('.ca') or
+                parsed_url.netloc.endswith('.jp') or
+                parsed_url.netloc.endswith('.nyc') or
+                parsed_url.netloc.endswith('.news') or
+                parsed_url.netloc.endswith('.de') or
+                parsed_url.netloc.endswith('.fr') or
+                parsed_url.netloc.endswith('.br') or
+                parsed_url.netloc.endswith('.me') or
+                parsed_url.netloc.endswith('.site') or
+                parsed_url.netloc.endswith('.cat') or
+                parsed_url.netloc.endswith('.tips') or
+                parsed_url.netloc.endswith('.fm') or
+                parsed_url.netloc.endswith('.by') or
+                parsed_url.netloc.endswith('.hk') or
+                parsed_url.netloc.endswith('.lt') or
+                parsed_url.netloc.endswith('.ph') or
+                parsed_url.netloc.endswith('.si') or
+                parsed_url.netloc.endswith('.rs') or
+                parsed_url.netloc.endswith('.ga') or
+                parsed_url.netloc.endswith('.la') or
+                parsed_url.netloc.endswith('.pro') or
+                parsed_url.netloc.endswith('.pl') or
+                parsed_url.netloc.endswith('.wiki') or
+                parsed_url.netloc.endswith('.info') or
                 parsed_url.netloc.endswith('.it') or
                 parsed_url.netloc.endswith('.co.uk')
             ):
